d4f054c5af824f052c694ce6f7bd11ecbfed8040
emre <emredilek6@gmail.com>
2022-08-03 23:46:09 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="app-container flex">
+		<div class='main-nav'>
+			<router-link class='nav-logo' to="/">Logo placeholder</router-link>
+			<router-link class='nav-page' to="/about">Breakfast</router-link>
+			<router-link class='nav-page' to="/about">Lunch</router-link>
+			<router-link class='nav-page' to="/about">Dinner</router-link>
+			<router-link class='nav-page' to="/about">Dessert</router-link>
+			<router-link class='nav-page' to="/about">Appetizers</router-link>
+			<div class='white-circle' />
+			<div class='white-circle' />
+		</div>
+		<router-view/>
+	</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style>
+html, body {
+	padding: 0;
+	margin: 0;
 }
 
-nav {
-  padding: 30px;
+.flex {display: flex}
+.grid {display: grid}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.app-container {}
+.main-nav {
+	display: grid;
+	background-color: #ffe1e1;
+	grid-gap: 1rem;
+	width: 18rem;
+	height: 100vh;
+	align-content: flex-start;
+	position: sticky;
+	top: 0;
+	font-size: 1.8rem;
+}
+.nav-logo {
+	margin-top: 5rem;
+}
+.nav-logo, .nav-page {
+	text-decoration: none;
+	color: black;
+	margin-left: 2rem;
+}
+.nav-page:nth-child(2) {margin-top: 3rem}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.white-circle {
+	width: 2rem;
+	height: 2rem;
+	border-radius: 50px;
+	background-color: white;
+	margin-left: 2rem;	
+}
+.white-circle:first-of-type {
+	margin-top: 5rem;
 }
 </style>
---
+++ src/views/RecipeView.vue
@@ -0,0 +1,69 @@
+<template>
+	<div className="recipe-page-container">
+		<h1>Salad</h1>
+		<p>Lorem ipsum dolor sit amet</p>
+		<div class="recipe-page-food-card flex">
+			<img class="food-card-image" :src="saladPic"/>
+			<div class="food-card-text grid">
+				<p>Prep: 5min</p>
+				<p>Cook: 5min</p>
+				<p>Total: 1hr35min</p>
+				<p>Servings: 8</p>
+				<p>Yields: 8 servings</p>
+			</div>
+		</div>
+		<div class="ingredients-wrapper">
+			<h1>Ingredients</h1>
+			<ol><li>Lorem</li><li>Ipsum</li><li>Dolor</li><li>Sit</li><li>Amet</li></ol>
+		</div>
+		<hr />
+		<div class="directions-wrapper">
+			<h1>Directions</h1>
+			<ol><li>Lorem</li><li>Ipsum</li><li>Dolor</li><li>Sit</li><li>Amet</li></ol>
+		</div>
+		<hr />
+		<div class="tips-wrapper">
+			<p>Tips:</p>
+			<p>Consider xyz lorem ipsum dolor sit amet</p>
+		</div>
+	</div>
+</template>
+<style>
+.recipe-page-container {
+	margin-left: 3rem;
+}
+.recipe-page-food-card {
+	background-color: #fff1f1;
+	width: calc(100vw - 20rem - 6rem);
+	border-radius: 10px
+}
+.food-card-image {
+	width: 20rem;
+	border-radius: 	10px 0 0 10px;
+}
+
+.food-card-text {
+	margin-left: 2.5rem;
+	align-content: center;
+	gap: 1.4rem;
+	font-size: 1.4rem;
+}
+.food-card-text p {margin: 0; padding: 0;}
+
+.ingredients-wrapper, .directions-wrapper, .tips-wrapper {margin-top: 3rem;}
+
+.ingredients-wrapper ol li, .directions-wrapper ol li {margin-top: .5rem;}
+
+hr {background-color: #7ecefece; border: none; height: 1px;}
+
+</style>
+<script>
+import saladPic from '@/assets/img/salad.jpg'
+export default {
+	data() {
+		return {
+			saladPic: saladPic
+		}
+	}
+}
+</script>
